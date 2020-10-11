ddfd12eebbbc2469ef9ecdda6e802f5de676cad0
Doru Foaltin <dorin.foaltin@hotmail.com>
2020-10-11 17:19:36 +0300

    Add project files.

---
+++ AgileHub.IntroToAzureDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AgileHub.IntroToAzureDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
